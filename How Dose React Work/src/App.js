e23c06ae10b10cfec0ab7404762d118d35ace44e
happai079 <gksyddl22@gmail.com>
2022-09-26 00:43:52 +0900

    컴포넌트와 real DOM

--- How Dose React Work/src/App.js
+++ How Dose React Work/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+	const [showParagraph, setShowParagraph] = useState(false);
+
+	const toggleParagraphHandler = () => {
+		setShowParagraph((prev) => !prev);
+	};
+
+	console.log('APP RUNNING');
+	return (
+		<div className="app">
+			<h1>Hi there</h1>
+			{showParagraph && <p>This is new!</p>}
+			<Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+		</div>
+	);
 }
 
 export default App;
