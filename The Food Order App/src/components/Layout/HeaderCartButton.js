5b21fb9209595ca8062db042954dd8c7b51d15ec
happai079 <gksyddl22@gmail.com>
2022-09-25 23:33:12 +0900

    header 장바구니 버튼에 애니메이션 효과

--- The Food Order App/src/components/Layout/HeaderCartButton.js
+++ The Food Order App/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from './../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 function HeaderCartButton(props) {
+	const [btnIsHiglighted, setBtnIsHighlighted] = useState(false);
 	const cartContext = useContext(CartContext);
 
 	const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
 		return curNumber + item.amount;
 	}, 0);
 
+	// 장바구니 항목수 변경시 버튼에 애니메이션 효과
+	const { items } = cartContext;
+	const btnClasses = `${classes.button} ${btnIsHiglighted ? classes.bump : ''}`;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+		setBtnIsHighlighted(true);
+
+		// 애니메이션 끝난 후 클래스 삭제
+		const timer = setTimeout(() => {
+			setBtnIsHighlighted(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
