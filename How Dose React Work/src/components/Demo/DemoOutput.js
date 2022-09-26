d7c182ffeb5f5c396ba5f26c90f40a22f2fff7fe
happai079 <gksyddl22@gmail.com>
2022-09-26 15:55:09 +0900

    React.memo로 불필요한 컴포넌트 재평가 방지

--- How Dose React Work/src/components/Demo/DemoOutput.js
+++ How Dose React Work/src/components/Demo/DemoOutput.js
@@ -2,7 +2,8 @@ import React from 'react';
 import MyParagraph from './MyParagraph';
 
 function DemoOutput(props) {
+	console.log('DemoOutput RUNNING');
 	return <MyParagraph>{props.show && 'This is new!'}</MyParagraph>;
 }
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
--- How Dose React Work/src/components/Demo/MyParagraph.js
+++ How Dose React Work/src/components/Demo/MyParagraph.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function MyParagraph(props) {
+	console.log('MyParagraph RUNNING');
 	return <p>{props.children}</p>;
 }
 
