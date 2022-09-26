de65573ef25e43b7e82b76e7f8a19596587d43b0
happai079 <gksyddl22@gmail.com>
2022-09-26 23:23:56 +0900

    useMemo로 데이터 저장하기 (최적화)

--- How Dose React Work/src/App.js
+++ How Dose React Work/src/App.js
@@ -1,30 +1,42 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 import './App.css';
+import DemoList from './components/Demo/DemoList';
 import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
-	const [showParagraph, setShowParagraph] = useState(false);
-	const [allowToggle, setAllowToggle] = useState(false);
+	// const [showParagraph, setShowParagraph] = useState(false);
+	// const [allowToggle, setAllowToggle] = useState(false);
 
-	const toggleParagraphHandler = useCallback(() => {
-		if (allowToggle) {
-			setShowParagraph((prev) => !prev);
-		}
-	}, [allowToggle]);
+	// const toggleParagraphHandler = useCallback(() => {
+	// 	if (allowToggle) {
+	// 		setShowParagraph((prev) => !prev);
+	// 	}
+	// }, [allowToggle]);
 
-	const allowToggleHandler = () => {
-		setAllowToggle(true);
-	};
+	// const allowToggleHandler = () => {
+	// 	setAllowToggle(true);
+	// };
+
+	const [listTitle, setListTitle] = useState('My List');
+
+	const changeTitleHandler = useCallback(() => {
+		setListTitle('New Title');
+	}, []);
+
+	const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
 	console.log('APP RUNNING');
+
 	return (
 		<div className="app">
-			<h1>Hi there</h1>
+			<DemoList title={listTitle} items={listItems} />
+			<Button onClick={changeTitleHandler}>Change List Title</Button>
+			{/* <h1>Hi there</h1>
 			<DemoOutput show={showParagraph} />
 			<Button onClick={allowToggleHandler}>Allow Toggling</Button>
-			<Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+			<Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button> */}
 		</div>
 	);
 }
